ad0a2bb793a4d9e8de940f3399922c743364cf4e
robin <robinlennox@users.noreply.github.com>
2016-07-17 17:16:11 +1200

    Fixed the Yahoo Search regex for domains. Simplified the code.

--- plugins/DomainEnum/YahooDomainEnum.py
+++ plugins/DomainEnum/YahooDomainEnum.py
@@ -4,6 +4,7 @@
 import re
 import urlparse
 
+from ..cleanup import *
 from ..processor import enumratorBaseThreaded
 from ..layout import *
 
@@ -13,37 +14,27 @@ G,Y,B,R,W = colour()
 class DomainSearch(enumratorBaseThreaded):
     def __init__(self, domain, subdomains=None, q=None):
         global verbose
+        global parsed_domain
+        parsed_domain = urlparse.urlparse(domain)
         verbose = subdomains
         subdomains = subdomains or []
         base_url = "https://search.yahoo.com/search?p={query}&b={page_no}"
         self.engine_name = "Yahoo"
         self.MAX_DOMAINS = 10
         self.MAX_PAGES = 0
-        super(DomainSearch, self).__init__(base_url, self.engine_name,domain, subdomains, q=q)
+        super(DomainSearch, self).__init__(base_url, self.engine_name, domain, subdomains, q=q)
         self.q = q
         return
 
     def extract_domains(self, resp):
-        link_regx2 = re.compile('<span class=" fz-15px fw-m fc-12th wr-bw.*?">(.*?)</span>')
-        link_regx = re.compile('<span class="txt"><span class=" cite fw-xl fz-15px">(.*?)</span>')
-        try:
-            links = link_regx.findall(resp)
-            links2 = link_regx2.findall(resp)
-            links_list = links+links2
-            for link in links_list:
-                link = re.sub("<(\/)?b>","", link)
-                if not link.startswith('http'):
-                    link="http://"+link
-                subdomain = urlparse.urlparse(link).netloc
-                if not subdomain.endswith(self.domain):
-                    continue
-                if subdomain and subdomain not in self.subdomains and subdomain != self.domain:
-                    if verbose:
-                        print "%s%s: %s%s"%(R, self.engine_name, W, subdomain)
-                    self.subdomains.append(subdomain)
-        except Exception as e:
-            pass
+        link_regx = re.compile('([^>]+).<b>'+parsed_domain.netloc.split('.')[0]+'</b>.*?'+parsed_domain.netloc.rsplit('.',1)[1]+'</b>')
+        links = link_regx.findall(resp)
+        links_list = links
 
+        for link in links_list:
+            # Check if span class is not in link
+            if "span class=" not in link:
+                clean_up_domain_text(parsed_domain,link,verbose,self)
         return links_list
 
     def should_sleep(self):
