b9c4ab31c71d9b90a76852de4768319a1803808e
robin <robinlennox@users.noreply.github.com>
2016-07-19 09:59:45 +1200

    Optimised text cleanup and domain extraction.

--- plugins/DomainEnum/AskDomainEnum.py
+++ plugins/DomainEnum/AskDomainEnum.py
@@ -3,6 +3,8 @@
 
 import re
 import urlparse
+
+from ..cleanup import *
 from ..processor import enumratorBaseThreaded
 from ..layout import *
 
@@ -12,6 +14,8 @@ G,Y,B,R,W = colour()
 class DomainSearch(enumratorBaseThreaded):
     def __init__(self, domain, subdomains=None, q=None):
         global verbose
+        global parsed_domain
+        parsed_domain = urlparse.urlparse(domain)
         verbose = subdomains
         subdomains = subdomains or []
         base_url = 'http://www.ask.com/web?q={query}&page={page_no}&qid=8D6EE6BF52E0C04527E51F64F22C4534&o=0&l=dir&qsrc=998&qo=pagination'
@@ -23,22 +27,13 @@ class DomainSearch(enumratorBaseThreaded):
         return
 
     def extract_domains(self, resp):
-        link_regx = re.compile('<p class="web-result-url">(.*?)</p>')
+        link_regx = re.compile('<p class="web-result-url">(.*?).'+parsed_domain.netloc)
         link_regx2 = re.compile('<p class="rightrail-web-result-url">(.*?)</p>')
-        try:
-            links = link_regx.findall(resp)
-            links2 = link_regx2.findall(resp)
-            links_list = links+links2
-            for link in links_list:
-                if not link.startswith('http'):
-                    link="http://"+link
-                subdomain = urlparse.urlparse(link).netloc
-                if subdomain not in self.subdomains and subdomain != self.domain:
-                    if verbose:
-                        print "%s%s: %s%s"%(R, self.engine_name, W, subdomain)
-                    self.subdomains.append(subdomain)
-        except Exception as e:
-            pass
+        links = link_regx.findall(resp)
+        links2 = link_regx2.findall(resp)
+        links_list = links+links2
+        for link in links_list:
+            clean_up_domain_text(parsed_domain,link,verbose,self)
 
         return links_list
 
