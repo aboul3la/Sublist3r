0f4d0b138861be2188f95a36bc22a6587a09f81f
robin <robinlennox@users.noreply.github.com>
2016-07-19 12:02:33 +1200

    Removed portscan from Passive DNS file.

--- plugins/DomainEnum/PassiveDNSDomainEnum.py
+++ plugins/DomainEnum/PassiveDNSDomainEnum.py
@@ -82,33 +82,3 @@ class DomainSearch(multiprocessing.Process):
                     self.subdomains.append(subdomain.strip())
         except Exception as e:
             pass
-
-class portscan():
-
-    def __init__(self,subdomains,ports):
-        self.subdomains = subdomains
-        self.ports = ports
-        self.threads = 20
-        self.lock = threading.BoundedSemaphore(value=self.threads)
-
-    def port_scan(self,host,ports):
-        openports = []
-        self.lock.acquire()
-        for port in ports:
-            try:
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.settimeout(2)
-                result = s.connect_ex((host, int(port)))
-                if result == 0:
-                    openports.append(port)
-                s.close
-            except Exception as e:
-                pass
-        self.lock.release()
-        if len(openports) > 0:
-            print "%s%s%s - %sFound open ports:%s %s%s%s"%(G,host,W,R,W,Y,', '.join(openports),W)
-
-    def run(self):
-        for subdomain in self.subdomains:
-            t = threading.Thread(target=self.port_scan,args=(subdomain,self.ports))
-            t.start()
