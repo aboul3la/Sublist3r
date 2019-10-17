8d58fa79f60728eef3c53f1e7af3bbf2f9a9c470
Verondrummond171 <56696878+Verondrummond171@users.noreply.github.com>
2019-10-17 15:48:37 -0500

    Create bannergrabber.py

---
+++ bannergrabber.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import socket
+
+
+
+def banner(ip, port)))
+s = socket.socket()
+s.connect((ip,int(port)))
+print(s.recv(2024)).strip('b')
+
+def main():
+       ip = input ("please enter ip:")
+        port = str(input("please enter port:"))
+        banner(ip, port)
+
+main()
+
+def banner():
+Green = '\033[92m'
+print(Green'+)(banner grabber)
