ba760fd3baf901cbd65135b0dead9359431ecb09
overoptimus <1040570917@qq.com>
2020-08-08 19:48:05 +0800

    python3中无unicode类型，修改报错

--- sublist3r.py
+++ sublist3r.py
@@ -300,7 +300,7 @@ class GoogleEnum(enumratorBaseThreaded):
         return links_list
 
     def check_response_errors(self, resp):
-        if (type(resp) is str or type(resp) is unicode) and 'Our systems have detected unusual traffic' in resp:
+        if (type(resp) is str or type(resp) is str) and 'Our systems have detected unusual traffic' in resp:
             self.print_(R + "[!] Error: Google probably now is blocking our requests" + W)
             self.print_(R + "[~] Finished now the Google Enumeration ..." + W)
             return False
