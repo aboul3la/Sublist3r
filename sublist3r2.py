95afaf1a5948ab0e5781ea527718fd68a2f56ac0
RoninNakomoto <nethunterlebanon@gmail.com>
2021-01-26 05:07:20 +0200

    fixed typos in help menu

--- sublist3r2.py
+++ sublist3r2.py
@@ -94,10 +94,10 @@ def parse_args():
     parser = argparse.ArgumentParser(epilog='\tExample: \r\npython ' + sys.argv[0] + " -d google.com")
     parser.error = parser_error
     parser._optionals.title = "OPTIONS"
-    parser.add_argument('-d', '--domain', help="Domain name to enumerate it's subdomains", required=True)
-    parser.add_argument('-b', '--bruteforce', help='Enable the subbrute bruteforce module', nargs='?', default=False)
-    parser.add_argument('-p', '--ports', help='Scan the found subdomains against specified tcp ports')
-    parser.add_argument('-v', '--verbose', help='Enable Verbosity and display results in realtime', nargs='?', default=False)
+    parser.add_argument('-d', '--domain', help="Domain name to use for subdomain enumeration", required=True)
+    parser.add_argument('-b', '--bruteforce', help='Turn-on aiodnsbrute bruteforce mode', nargs='?', default=False)
+    parser.add_argument('-p', '--ports', help='Check/Filter subdomain results for open ports (provide comma separated ports')
+    parser.add_argument('-v', '--verbose', help='Enable verbose mode and display results in realtime', nargs='?', default=False)
     parser.add_argument('-t', '--threads', help='Number of threads to use for aiodnsbrute bruteforce', type=int, default=7000)
     parser.add_argument('-e', '--engines', help='Specify a comma-separated list of search engines')
     parser.add_argument('-o', '--output', help='Save the results to text file')
