02d737549c00e8464b545bc79a30a70f555bf94c
Kaustubh BM <kaustubhbm3574@gmail.com>
2023-02-06 12:10:14 +0530

    Update sublist3r.py

--- sublist3r.py
+++ sublist3r.py
@@ -99,6 +99,7 @@ def parse_args():
     parser.add_argument('-b', '--bruteforce', help='Enable the subbrute bruteforce module', nargs='?', default=False)
     parser.add_argument('-p', '--ports', help='Scan the found subdomains against specified tcp ports')
     parser.add_argument('-v', '--verbose', help='Enable Verbosity and display results in realtime', nargs='?', default=False)
+    parser.add_argument('-s', '--silent', help='Disable Verbosity and run the script silently', nargs='?', default=False)
     parser.add_argument('-t', '--threads', help='Number of threads to use for subbrute bruteforce', type=int, default=30)
     parser.add_argument('-e', '--engines', help='Specify a comma-separated list of search engines')
     parser.add_argument('-o', '--output', help='Save the results to text file')
